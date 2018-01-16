85b323442221cf06276baca29ef7694465280744
Gavin Epstein <gepstein@cs.oberlin.edu>
2018-01-16 18:14:58 -0500

    lighting, but there's a tuple of errors

--- Makefile
+++ Makefile
@@ -6,9 +6,12 @@ MyArea.o: MyArea.cpp
 fractal.o: fractal.cpp
 	g++ -std=c++14 `pkg-config --cflags --libs gtkmm-3.0` -c fractal.cpp
 
-lightingtest: lightingtest.cpp vec4.o
-        g++ -std=c++14 lightingtest.cpp -o lightingtest vec4.o
+lightingtest: lightingtest.cpp vec4.o light.o
+        g++ -std=c++14 lightingtest.cpp -o lightingtest vec4.o light.o
 vec4.o: vec4.cpp
         g++ -std=c++14 -c vec4.cpp 
+
+light.o: light.cpp
+	g++ -std=c++14 -c light.cpp
 clean:
-	rm MyArea.o fractal.o main lightingtest vec4.o
+	rm MyArea.o fractal.o main lightingtest vec4.o light.o
--- vec4.h
+++ vec4.h
@@ -1,3 +1,7 @@
+#ifndef VEC_FOUR_H
+#define VEC_FOUR_H
+
+
 
 //4d vector with integer values
 class Vec4{
@@ -11,16 +15,21 @@ class Vec4{
   Vec4(int x, int y, int z, int w);
   // vector addition
   friend Vec4 operator+( Vec4 vec1,  Vec4 vec2);
+  //and subtractio
+  friend Vec4 operator-(Vec4 vec1, Vec4 vec2);
+
   //normalizes w to 1 then takes the cross product over x,y,z
   Vec4 cross( Vec4 vec2);
   // scales down by w
-  void normalize ();
+  void normalize_w ();
   // normalizes and takes the dot product
   int dot(Vec4 vec2);
   //copies
   Vec4 copy();
   //scale
   void scale(int f);
+  //magnitude, ignoring w
+  int magnitude();
 };
 
 
@@ -36,15 +45,23 @@ class Vec4f{
   Vec4f(float x, float y, float z, float w);
   // vector addition                                                                                                                                            
   friend Vec4f operator+( Vec4f vec1,  Vec4f vec2);
+
+  friend Vec4f operator-(Vec4f vec1, Vec4f vec2);
   //normalizes w to 1 then takes the cross product over x,y,z                                                                                                   
   Vec4f cross( Vec4f vec2);
-  // scales down by w                                                                                                                                           
-  void normalize ();
-  // normalizes and takes the dot product                                                                                                                       
+  // scales down by w
+  void normalize_w ();
+  // scales down by magnitude
+  void normalize();
+
+  // normalizes and takes the dot product  
   double dot(Vec4f vec2);
   //copies                                                                                                                                                      
   Vec4f copy();
-  //scale                                                                                                                                                       
-  void scale(int f);
+  //scale         
+  void scale(double f);
+  //magnitude, ignoring w
+  double magnitude();
 };
 
+#endif
